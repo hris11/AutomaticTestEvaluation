4d991e94d76382c07d49a9aaadb3e46cc5d70e52
hris11 <hris11@abv.bg>
2017-11-10 23:25:15 +0200

    deleting some of the app.js component

--- automatic-test-evaluation/src/App.js
+++ automatic-test-evaluation/src/App.js
@@ -43,25 +43,11 @@ class App extends Component {
             error = (<p>Error: {this.state.error}</p>);
         }
         return (
-                <div>
-                    <div>
-                    <RegisterLoginComponent/>
-                    <ClassCreateComponent/>
-                    </div>
-                    <div className="App">
-                        <header className="App-header">
-                            <img src={logo} className="App-logo" alt="logo" />
-                            <h1 className="App-title">Welcome to React</h1>
-                        </header>
-                        <p className="App-intro">
-                            To get started, edit <code>src/App.js</code> and save to reload.
-                        </p>
-                        {data}
-                        {error}
-                        <button onClick={this.makeAjaxRequest.bind(this, '/rest/hello')} >Click me</button>
-                    </div>
-                </div>
-                );
+            <div>
+            <RegisterLoginComponent/>
+            <ClassCreateComponent/>
+            </div>
+        );
     }
 }
 
